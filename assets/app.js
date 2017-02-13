101e558ff3278c7fe3bde98cd12ab2f3a2d8eed8
Daniel Vianna <dvianna@bunnings.com.au>
2017-02-14 07:51:10 +0800

    React works on frontend; no communication yet

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+
+const GraphDashboard = React.createClass({
+  render: function () {
+    return (
+      <h1>insert graph here</h1>
+    )
+  }
+})
+
+ReactDOM.render(
+  <GraphDashboard />,
+  document.getElementById('content')
+)
